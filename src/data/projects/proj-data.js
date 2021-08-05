657033d9f76087bd820564bc72786ad6fd94a24a
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-08-05 14:20:24 -0500

    Add placeholder images, tweak responsiveness

--- src/data/projects/proj-data.js
+++ src/data/projects/proj-data.js
@@ -5,7 +5,7 @@ export const currentProject = [
   teamQuant: 1,
   stack: ['JavaScript', 'CSS3', 'HTML5', 'Cypress', 'React', 'TypeScript', 'Jest'],
   abst: "In the early planning phases of an app to track and submit work orders for Facilities Maintenance teams across multiple sites. Would allow employees to submit work orders and any maintenance techs to see orders across all sites, make notes, view maps, and provide live updates to submitters. Currently in the design/wireframe process.",
-  gal: {photos: ["/Peaky-copy.png"], alt: ["../%PUBLIC_URL%/Peaky-copy.png"]},
+  gal: {photos: ["/projects/placeholders/2.png"], alt: ["Meet my cat Peaky! She sits here when no project photos are available."]},
   isDeployed: false,
   site: null,
   codeRepo: null
