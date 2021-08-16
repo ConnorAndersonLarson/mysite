35b640b554213d1907021796a5a4ce4dc9e0c7a3
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-08-16 06:49:43 -0500

    Left align about me section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,8 +11,8 @@ function About() {
         <section className="about">
           <p>Combining 5 years of problem solving and process optimization with a lifelong creative passion, I’m a software developer looking to utilize my expertise to design and implement efficient, creative user experiences.</p>
           <p><br />On the human side of things I love being outdoors, biking, rock climbing, spending time with my wife, woodworking, and cooking/baking both for myself and for the workers at the local businesses around me.</p>
-          <p><br /> I am driven by providing a better life for my wife and cats, lowering barriers to entry in all things I enjoy, and helping those around me have opportunities to excel at whatever they're passionate about.</p>
-          <p><br /> If you want to talk shop (wood or work), share recipes, geek out over bicycles/cats/movies/etc. feel free to reach out by email at:</p>
+          <p><br />I am driven by providing a better life for my wife and cats, lowering barriers to entry in all things I enjoy, and helping those around me have opportunities to excel at whatever they're passionate about.</p>
+          <p><br />If you want to talk shop (wood or work), share recipes, geek out over bicycles/cats/movies/etc. feel free to reach out by email at:</p>
           <p><b>connorandersonlarson@gmail.com</b></p>
         </section>
         <aside className="skills">
